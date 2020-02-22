a7729896bfb695ab737bdf93dd8e2c58d9c810f0
Max <IMbrera@users.noreply.github.com>
2020-02-22 20:46:49 +0300

    Created Entities

---
+++ Entities/Feed.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+  public  class Feed
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public DateTime PublicDate { get; set; }
+
+        public int PlaceID { get; set; }
+        public Place Place { get; set; }
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+       // public byte logo { get; set; }
+       
+
+    }
+}
---
+++ Entities/Place.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+ public   class Place
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Coords { get; set; }
+    }
+}
